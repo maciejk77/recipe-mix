a9dad218f9d62da15fd255523d010981b15f2a19
Maciej Kurek <maciejk77@gmail.com>
2023-05-12 14:07:56 +0100

    Refactor, layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 // import { useState } from "react";
 // import { useQuery, gql } from "@apollo/client";
 // import { useDebounce } from "use-debounce";
-// import Layout from "src/components/layout";
+import Layout from "src/components/layout";
 // import RecipesList from "src/components/recipesList";
 // import { useLastData } from "src/utils/useLastData";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { RecipesQuery, RecipesQueryVariables } from "src/generated/RecipesQuery";
 
 export default function Home() {
-  return <div>Home</div>;
+  return <Layout main={<div>Home</div>} />;
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,45 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import { FunctionComponent, ReactNode } from "react";
+import Link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+interface IProps {
+  main: ReactNode;
+}
+
+const Layout: FunctionComponent<IProps> = ({ main }) => {
+  const authenticated = false;
+  const logout = () => null;
+
+  return (
+    <div className="bg-gray-400 max-w-screen-2xl mx-auto text-white">
+      <nav className="bg-gray-600" style={{ height: "64px" }}>
+        <div className="px-3 flex items-center justify-between h-16">
+          <Link href="/">
+            <a>
+              <img
+                src="/cropped-favicon.png"
+                alt="recipe icon"
+                className="inline w-12"
+              />
+            </a>
+          </Link>
+          {authenticated ? (
+            <>
+              <Link href="/recipes/add">
+                <a>Add Recipe</a>
+              </Link>
+              <button onClick={logout}>Logout</button>
+            </>
+          ) : (
+            <Link href="/auth">
+              <a>Login | Signup</a>
+            </Link>
+          )}
+        </div>
+      </nav>
+      <main style={{ minHeight: "calc(100vh-64px)" }}>{main}</main>
+    </div>
+  );
+};
+
+export default Layout;
